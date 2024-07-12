4b03d40c1cfbd2ae4d56cc3462283577d9a94e04
preedok <edo.vivoy21@gmail.com>
2024-07-13 02:22:30 +0700

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  // server: {
-  //   port: 3001,
-  // },
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      keyframes: {
-        floating: {
-          '0%, 100%': { transform: 'translateY(-3deg)' },
-          '50%': { transform: 'translateY(3deg)' },
-        },
-      },
-      animation: {
-        floating: 'floating 3s infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
-});
+}
